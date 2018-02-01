7dd5cac94a89c4128835b347985817f6e3d5eaf4
aruncharan <35652114+aruncharan@users.noreply.github.com>
2018-02-01 16:14:20 +0530

    Create number of digit.c

---
+++ number of digit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+  long long a;
+  int o;
+  printf("enter the integer");
+  scanf("ll%d", &a);
+  while (a !=0)
+  {
+    n / =10; ++10;
+  }
+  printf(" number of digit %d",  count);
+}
