85216c82db0177ce914a81175e4aeea4872abc0b
aruncharan <35652114+aruncharan@users.noreply.github.com>
2018-02-01 16:28:57 +0530

    Create natural number.c

---
+++ natural number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+  int a,b, sum =0;
+  printf(" enter the a value");
+  scanf("%d",&b);
+  for(b=1,b <=0,b++);
+  {
+   sum + =1;
+   }
+   printf("sum =%d",sum);
+ }
