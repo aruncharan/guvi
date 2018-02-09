12b48c9198c1aa907b1ac231e44353a9e70a32ad
aruncharan <35652114+aruncharan@users.noreply.github.com>
2018-02-09 16:32:18 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,i,d=1;
+    printf("enter the a value  ");
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        d=d*i;
+    }
+        printf("\n %d",d );
+   
+}
+   
