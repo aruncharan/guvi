8e20999621dd8610e9f31102dc3c65041d9e764f
aruncharan <35652114+aruncharan@users.noreply.github.com>
2018-02-09 16:22:42 +0530

    Create given number multuiples.c

---
+++ given number multuiples.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,i;
+    printf("enter the a value  ");
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        b=i*a;
+        printf("\n %d", b);
+    }
+}
+   
