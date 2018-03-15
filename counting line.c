d59ff697433257eba2eb5644916bf1be969372e4
aruncharan <35652114+aruncharan@users.noreply.github.com>
2018-03-15 16:01:20 +0530

    Create counting line.c

---
+++ counting line.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    char a[100];
+    int b,i,c;
+    printf("enter the character value ");
+    scanf("%[^\n]",&a);
+    for(i=1;a[i]!='\0';i++)
+    {
+        if(a[i]=='.')
+        c++;
+    }
+    printf("the total number of word =%d",c+1);
+}
